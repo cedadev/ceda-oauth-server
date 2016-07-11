a30edc8ac7ce3e801d9043f7efa5238783f6a217
Matt Pryor <matt.pryor@stfc.ac.uk>
2016-07-11 13:38:05 +0100

    Changes to allow OPENID to be passed in WSGI environ

--- ceda_oauth_server/urls.py
+++ ceda_oauth_server/urls.py
@@ -14,26 +14,36 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
 from paste.deploy import loadapp
-from django_wsgi.embedded_wsgi import make_wsgi_view
+from django_wsgi.embedded_wsgi import call_wsgi_app
 from oauth2_provider.decorators import protected_resource
 from oauth2_provider import views as oauth_views
+
+from userdb_model.models import User
 
 
 # Use Paste Deploy to create the online ca app
 onlineca_app = loadapp(settings.ONLINECA_PASTEDEPLOY_CONF)
-# Create a Django view from the WSGI app
-onlineca_app = make_wsgi_view(onlineca_app)
-# Make the app exempt from CSRF
-onlineca_app = csrf_exempt(onlineca_app)
-# Protect the resource with OAuth
-onlineca_app = protected_resource(scopes = [settings.CERTIFICATE_SCOPE])(onlineca_app)
+
+@protected_resource(scopes = [settings.CERTIFICATE_SCOPE])
+@csrf_exempt
+def onlineca_view(request, path_info):
+    # Try to retrieve the openid for the user
+    if request.user.is_authenticated():
+        try:
+            ceda_user = User.objects.get(accountid = request.user.username)
+        except User.DoesNotExist:
+            pass
+        else:
+            request.environ['OPENID'] = ceda_user.openid
+    return call_wsgi_app(onlineca_app, request, path_info)
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # The OnlineCA app provides oauth/trustroots/ and oauth/certificate/
     # This pattern can't be in the include below as we need to capture the
     # leading slash to pass to the underlying WSGI app
-    url(r'^oauth(/trustroots/|/certificate/)$', onlineca_app, name = 'ca_server'),
+    url(r'^oauth(/trustroots/|/certificate/)$', onlineca_view, name = 'ca_server'),
     # We only provide the token-issuing OAuth URLs, and at custom endpoints
     # Applications are managed only via the admin interface
     url(r'^oauth/', include([
